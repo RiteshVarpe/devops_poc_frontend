ac2b9a6b7a3ef5cd1c42ec51d316872e40b68d93
rohini.kumari <rohini.kumari@afourtech.com>
2022-07-04 15:04:45 +0530

    modified length column t0o course-length

--- src/components/course/CourseList.js
+++ src/components/course/CourseList.js
@@ -74,7 +74,7 @@ class CourseList extends React.Component {
                     caretRender={getCaret}
                     columnTitle
                 >
-                    length
+                    course-length
                 </TableHeaderColumn>
 
                 <TableHeaderColumn 
